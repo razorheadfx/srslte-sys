0724fee0c16a27ef830dcfe3dc113fad8efc4924
razorheadfx <razorhead.effect@gmail.com>
2017-11-06 15:19:49 +0100

    Shorten lib namespace

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-pub mod srslte {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
